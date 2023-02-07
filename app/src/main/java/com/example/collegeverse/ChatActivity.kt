5a23260ea84662dcf429fb6ec6e7d681a6932904
Dharma-Raghava-Sai-Shashank <104642011+Dharma-Raghava-Sai-Shashank@users.noreply.github.com>
2023-02-07 17:10:48 +0530

    dfg

---
+++ app/src/main/java/com/example/collegeverse/ChatActivity.kt
@@ -0,0 +1,14 @@
+package com.example.collegeverse
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.collegeverse.databinding.ActivityChatBinding
+
+class ChatActivity : AppCompatActivity() {
+    
+}
